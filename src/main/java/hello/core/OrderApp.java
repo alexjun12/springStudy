08abb86da58f2c7009b58688a4fd882b18b438d3
alexjun12 <alexjun12@naver.com>
2022-08-14 22:54:56 +0900

    create AppConfig file(interest separation)

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -3,19 +3,20 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceimpl;
+import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberServeice = new MemberServiceimpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId,"memberA", Grade.VIP);
-        memberServeice.join(member);
+        memberService.join(member);
 
         Order order = orderService.createOrder(memberId,"itemA",10000);
         System.out.println("order = "+ order);
